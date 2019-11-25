148c300723f15cb48eba7bfc5d61db410ec32205
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2019-11-25 23:43:56 +0100

    refactor: preliminary tidies to the whole conformance test suite

---
+++ apps/omg/test/omg/conformance/signature_test.exs
@@ -0,0 +1,117 @@
+# Copyright 2019 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule OMG.Conformance.SignatureTest do
+  @moduledoc """
+  Tests that EIP-712-compliant signatures generated `somehow` (via Elixir code as it happens) are treated the same
+  by both Elixir signature code and contract signature code.
+  """
+
+  alias OMG.DevCrypto
+  alias OMG.State.Transaction
+
+  import Support.Conformance
+
+  use Support.Conformance.Case, async: false
+
+  @moduletag :integration
+  @moduletag :common
+
+  @alice <<1::160>>
+  @bob <<2::160>>
+  @eth OMG.Eth.RootChain.eth_pseudo_address()
+  @token <<3::160>>
+  @zero_address OMG.Eth.zero_address()
+
+  describe "elixir vs solidity conformance test" do
+    test "signature test empty transaction", %{contract: contract} do
+      tx = Transaction.Payment.new([], [])
+      verify(contract, tx)
+    end
+
+    test "no inputs test", %{contract: contract} do
+      tx = Transaction.Payment.new([], [{@alice, @eth, 100}])
+      verify(contract, tx)
+    end
+
+    test "no outputs test", %{contract: contract} do
+      tx = Transaction.Payment.new([{1, 0, 0}], [])
+      verify(contract, tx)
+    end
+
+    test "signature test - small tx", %{contract: contract} do
+      tx = Transaction.Payment.new([{1, 0, 0}], [{@alice, @eth, 100}])
+      verify(contract, tx)
+    end
+
+    test "signature test - full tx", %{contract: contract} do
+      tx =
+        Transaction.Payment.new(
+          [{1, 0, 0}, {1000, 555, 3}, {2000, 333, 1}, {15_015, 0, 0}],
+          [{@alice, @eth, 100}, {@alice, @token, 50}, {@bob, @token, 75}, {@bob, @eth, 25}]
+        )
+
+      verify(contract, tx)
+    end
+
+    test "signature test transaction with metadata", %{contract: contract} do
+      {:ok, <<_::256>> = metadata} = DevCrypto.generate_private_key()
+
+      tx =
+        Transaction.Payment.new(
+          [{1, 0, 0}, {1000, 555, 3}, {2000, 333, 1}, {15_015, 0, 0}],
+          [{@alice, @eth, 100}, {@alice, @eth, 50}, {@bob, @eth, 75}, {@bob, @eth, 25}],
+          metadata
+        )
+
+      verify(contract, tx)
+    end
+
+    test "signature test, transaction with zero output amount", %{contract: contract} do
+      tx = Transaction.Payment.new([{1, 0, 0}], [{@alice, @eth, 100}, {<<1::160>>, @zero_address, 0}])
+      verify(contract, tx)
+    end
+
+    test "signature test, transaction with an explicit zero output", %{contract: contract} do
+      tx = Transaction.Payment.new([{1, 0, 0}], [{@alice, @eth, 100}, {@zero_address, @zero_address, 0}])
+      verify(contract, tx)
+    end
+
+    test "signature test, transaction with an explicit zero input", %{contract: contract} do
+      tx = Transaction.Payment.new([{1, 0, 0}, {0, 0, 0}], [{@alice, @eth, 100}])
+      verify(contract, tx)
+    end
+  end
+
+  # FIXME: this might not belong here, technically speaking it could cover the same stuff if put in `plasma_contracts`
+  describe "distinct transactions yield distinct sign hashes" do
+    test "sanity check - different txs hash differently", %{contract: contract} do
+      tx1 = Transaction.Payment.new([{1, 0, 0}], [{@alice, @eth, 100}])
+      tx2 = Transaction.Payment.new([{2, 0, 0}], [{@alice, @eth, 100}])
+      verify_distinct(contract, tx1, tx2)
+    end
+
+    test "explicit zero input alters sign hash", %{contract: contract} do
+      tx1 = Transaction.Payment.new([{1, 0, 0}], [{@alice, @eth, 100}])
+      tx2 = Transaction.Payment.new([{1, 0, 0}, {0, 0, 0}], [{@alice, @eth, 100}])
+      verify_distinct(contract, tx1, tx2)
+    end
+
+    test "explicit zero outputs alters sign hash", %{contract: contract} do
+      tx1 = Transaction.Payment.new([{1, 0, 0}], [{@alice, @eth, 100}])
+      tx2 = Transaction.Payment.new([{1, 0, 0}], [{@alice, @eth, 100}, {@zero_address, @zero_address, 0}])
+      verify_distinct(contract, tx1, tx2)
+    end
+  end
+end
---
+++ apps/omg/test/support/conformance/case.ex
@@ -0,0 +1,50 @@
+# Copyright 2019 OmiseGO Pte Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+defmodule Support.Conformance.Case do
+  @moduledoc """
+  `ExUnit` test case for the setup required by a test of Elixir and Solidity implementation conformance
+  """
+  use ExUnit.CaseTemplate
+
+  alias OMG.Eth
+  alias Support.Deployer
+  alias Support.DevNode
+
+  setup_all do
+    {:ok, exit_fn} =
+      if Application.get_env(:omg_eth, :run_test_eth_dev_node, true) do
+        DevNode.start()
+      else
+        {:ok, fn -> :ok end}
+      end
+
+    root_path = Application.fetch_env!(:omg_eth, :umbrella_root_dir)
+    {:ok, [addr | _]} = Ethereumex.HttpClient.eth_accounts()
+
+    {:ok, _, signtest_addr} = Deployer.create_new("PaymentEip712LibMock", root_path, Eth.Encoding.from_hex(addr), [])
+
+    # impose our testing signature contract wrapper (mock) as the validating contract, which normally would be
+    # plasma framework
+    :ok = Application.put_env(:omg_eth, :contract_addr, %{plasma_framework: Eth.Encoding.to_hex(signtest_addr)})
+
+    on_exit(fn ->
+      # reverting to the original values from `omg_eth/config/test.exs`
+      Application.put_env(:omg_eth, :contract_addr, %{plasma_framework: "0x0000000000000000000000000000000000000001"})
+      exit_fn.()
+    end)
+
+    [contract: signtest_addr]
+  end
+end
